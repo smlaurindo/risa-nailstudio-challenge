d92715e2e8a4a69b725f52f3c619df081e780565
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2026-02-03 11:34:47 -0300

    Initial Commit

---
+++ src/main/java/com/smlaurindo/risanailstudio/RisaNailStudioApiApplication.java
@@ -0,0 +1,11 @@
+package com.smlaurindo.risanailstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisaNailStudioApiApplication {
+	void main(String[] args) {
+		SpringApplication.run(RisaNailStudioApiApplication.class, args);
+	}
+}
